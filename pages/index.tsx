22f851f1ee45c23a0ca482296ee559a77af095b2
Stephano Lioe <sraoullioeanjie@anwb.nl>
2022-07-06 10:59:23 +0200

    add: supabase magic link auth without styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react'
+import { Session } from '@supabase/supabase-js'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { Auth } from '../components/auth'
 import { KanbanBoard } from '../components/kanban-board'
 import { mockData } from '../data/mockData'
+import { supabase } from '../utils/supabaseClient'
 
 const Home: NextPage = () => {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  if (!session) {
+    return <Auth />
+  }
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+      console.warn('Could not sign out')
+    }
+  }
+
   return (
     <>
       <Head>
@@ -12,6 +37,14 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div className='h-screen'>
+        <div>
+          <button
+            className='h-9 rounded-xl bg-red-300 drop-shadow-sm duration-300 hover:bg-red-200 px-4'
+            onClick={signOut}
+          >
+            Sign out
+          </button>
+        </div>
         <KanbanBoard kanbanState={mockData} />
       </div>
     </>
