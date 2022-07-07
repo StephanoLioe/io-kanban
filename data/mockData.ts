dae3723a7f04f89945de5a05b9454d23cc66b774
Stephano Lioe <34340931+StephanoLioe@users.noreply.github.com>
2022-07-07 13:11:16 +0200

    Feature/draggable kanban board (#1)
    * add: react-beautifull-dnd
    
    * remove: next js initial markup  index page
    
    * add: ui kanban components
    
    * add: types react-beatifull-dnd
    
    * implement: drag and drop cards
    
    * add: link dribble design in readme
    
    * implement: review feedback
    
    Co-authored-by: Stephano Lioe <sraoullioeanjie@anwb.nl>

--- data/mockData.ts
+++ data/mockData.ts
@@ -1,36 +1,36 @@
 export const mockData = {
   columns: [
     {
-      id: 'column-1',
+      id: 1,
       title: 'To do',
       position: 0,
       tasks: [
         {
-          id: '1',
+          id: 1,
           title: 'Design',
           content: 'Make a awesome design',
           position: 1,
         },
         {
-          id: '2',
+          id: 2,
           title: 'Develop',
           content: 'Develop an awesome app',
           position: 2,
         },
         {
-          id: '3',
+          id: 3,
           title: 'Research',
           content: 'What kind of statemanagement will be used?',
           position: 3,
         },
         {
-          id: '4',
+          id: 4,
           title: 'Delegate',
           content: 'Let co-pilot build the whole app',
           position: 4,
         },
         {
-          id: '5',
+          id: 5,
           title: 'Verify',
           content: 'if all the cerificates are in',
           position: 5,
@@ -38,12 +38,12 @@ export const mockData = {
       ],
     },
     {
-      id: 'column-2',
+      id: 2,
       title: 'In progress',
       position: 1,
       tasks: [
         {
-          id: '6',
+          id: 6,
           title: 'Waiting on',
           content: 'approvals to go live',
           position: 0,
@@ -51,7 +51,7 @@ export const mockData = {
       ],
     },
     {
-      id: 'column-3',
+      id: 3,
       title: 'Completed',
       position: 2,
       tasks: [],
